5e5c599a318009ec2533a65d91fcb85b7e634812
Paweł Nąckiewicz <p.nackiewicz@student.po.edu.pl>
2017-12-15 12:40:36 +0100

    v0.1
    Add subpages
    Add views
    Add controllers
    Modify routing system

---
+++ CodeTogether/Controllers/UserDashboardController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeTogether.Controllers
+{
+    public class UserDashboardController : Controller
+    {
+        // GET: UserDashboard
+        [Route("UserDashboard")]
+        public ActionResult UserDashboardIndex()
+        {
+            return View();
+        }
+    }
+}
