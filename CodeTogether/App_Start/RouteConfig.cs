cbd7f336624ca9829648caaf235b0a10bdfe0932
Paweł Nąckiewicz <p.nackiewicz@student.po.edu.pl>
2017-12-15 12:25:55 +0100

    v0.1 (summary)
    Add subpages
    Add views
    Add controlers
    Modify routing system
    (description)

--- CodeTogether/App_Start/RouteConfig.cs
+++ CodeTogether/App_Start/RouteConfig.cs
@@ -13,11 +13,42 @@ namespace CodeTogether
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+              name: "Login",
+              url: "Login/{action}/{id}",
+              defaults: new { controller = "Login", action = "LoginIndex", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+              name: "Register",
+              url: "Register/{action}/{id}",
+
+              defaults: new { controller = "Register", action = "RegisterIndex", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+             name: "UserDashboard",
+             url: "UserDashboard/{action}/{id}",
+
+             defaults: new { controller = "UserDashboard", action = "UserDashboardIndex", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+             name: "MyProfile",
+             url: "MyProfile/{action}/{id}",
+
+             defaults: new { controller = "MyProfile", action = "MyProfileIndex", id = UrlParameter.Optional }
+            );
+
         }
+
     }
 }
